a3aedc33270dfb473c2257a7f2bf3972696672dc
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2020-10-19 17:35:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+	paper = new Paper(200,600,40);
+	ground = new Ground(800,690,1600,70);
+
+	binr = new Dustbin(1200,610);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();    
+  paper.display();
+  ground.display();
+  
+  binr.display();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	   Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-145});   
+	 }
+   } 
+ 
