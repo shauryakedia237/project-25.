2a56b79732734c8c077a651acd2b01ac38c3ad39
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2020-10-23 19:33:41 +0530

    hiiiiiii

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-	   Matter.Body.applyForce(paper.body,paper.body.position,{x:75,y:-100});   
+	   Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-100});   
 	 }
    } 
  
